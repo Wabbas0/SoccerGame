7cbf7fc723ee1ba6589f73a9db0f57b3d57de564
Walid <wibrahim@viktor.ai>
2023-09-06 20:58:48 +0200

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+};
